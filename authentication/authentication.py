dd377778e3bccc5a75a2526188cc9f3cef6a2f26
Gyoza-Gyoza <136580712+Gyoza-Gyoza@users.noreply.github.com>
2026-06-09 21:42:36 +0800

    Revert "Update authentication.py"
    This reverts commit f58a1ca49e3a1d377979e0eaa512ce3e805eb9a8.

--- authentication/authentication.py
+++ authentication/authentication.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter
 from pydantic import BaseModel
 from database import get_connection
 
@@ -14,70 +14,77 @@ class User(BaseModel):
     first_name: str
     last_name: str
 
-@router.post(
-    "/users",
-        status_code=status.HTTP_201_CREATED
-)
+@router.post("/create_user")
 def create_user(user: User):
-    with get_connection() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("""
-            INSERT INTO users
-            (username, password, email, first_name, last_name)
-            VALUES (%s, %s, %s, %s, %s)""",
-                           (user.username, user.password, user.email, user.first_name, user.last_name))
+    conn = get_connection()
+    cursor = conn.cursor()
 
-            return {f"message": f"{user.username} created successfully"}
+    cursor.execute("""
+    INSERT INTO users
+    (username, password, email, first_name, last_name)
+    VALUES (%s, %s, %s, %s, %s)""",
+                   (user.username, user.password, user.email, user.first_name, user.last_name))
 
-@router.get(
-    "/users",
-    status_code=status.HTTP_200_OK
-)
+    conn.commit()
+    conn.close()
+    return {f"message": f"{user.username} created successfully"}
+
+@router.get("/users")
 def get_users():
-    with get_connection() as conn:
-        with conn.cursor as cursor:
-            cursor.execute("""
-            SELECT username, password, email, first_name, last_name FROM users""")
+    conn = get_connection()
+    cursor = conn.cursor()
 
-            users = cursor.fetchall()
-            return users
+    cursor.execute("""
+    SELECT username, password, email, first_name, last_name FROM users""")
+
+    users = cursor.fetchall()
+    conn.close()
+    return users
 
 @router.get("/users/{user_id}")
 def get_user(user_id: str):
-    with get_connection() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("""
-            SELECT username, password, email, first_name, last_name FROM users WHERE user_id = %(id)s""", {'id': user_id})
-            user = cursor.fetchone()
+    conn = get_connection()
+    cursor = conn.cursor()
 
-            return user
+    cursor.execute("""
+    SELECT username, password, email, first_name, last_name FROM users WHERE user_id = %(id)s""", {'id': user_id})
+    user = cursor.fetchone()
 
-@router.put("/user/{user_id}")
+    conn.close()
+    return user
+
+@router.put("/update_user/{user_id}")
 def update_user(user_id: str, user: User):
-    with get_connection() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("""
-            UPDATE users set username= %(username)s, 
-            password= %(password)s, 
-            email= %(email)s, 
-            first_name= %(first_name)s, 
-            last_name= %(last_name)s
-            WHERE user_id = %(id)s""",
-                           {'id': user_id,
-                            'first_name': user.first_name,
-                            'last_name': user.last_name,
-                            'email': user.email,
-                            'username': user.username,
-                            'password': user.password})
+    conn = get_connection()
+    cursor = conn.cursor()
 
-            return {f"message": f"{user.username} updated successfully"}
+    cursor.execute("""
+    UPDATE users set username= %(username)s, 
+    password= %(password)s, 
+    email= %(email)s, 
+    first_name= %(first_name)s, 
+    last_name= %(last_name)s
+    WHERE user_id = %(id)s""",
+                   {'id': user_id,
+                    'first_name': user.first_name,
+                    'last_name': user.last_name,
+                    'email': user.email,
+                    'username': user.username,
+                    'password': user.password})
 
-@router.delete("/user/{user_id}")
+    conn.commit()
+    conn.close()
+
+    return {f"message": f"{user.username} updated successfully"}
+
+@router.delete("/delete_uesr/{user_id}")
 def delete_user(user_id: str):
-    with get_connection() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("""
-            DELETE FROM users WHERE user_id = %(id)s""", {'id': user_id})
+    conn = get_connection()
+    cursor = conn.cursor()
 
-            conn.commit()
-            return {"message": f"{user_id} deleted successfully"}
+    cursor.execute("""
+    DELETE FROM users WHERE user_id = %(id)s""", {'id': user_id})
+
+    conn.commit()
+    conn.close()
+    return {"message": f"{user_id} deleted successfully"}
