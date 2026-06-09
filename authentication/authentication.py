54b909601a3b84b90d1266f2fc0bd0d0487613cf
Gyoza-Gyoza <136580712+Gyoza-Gyoza@users.noreply.github.com>
2026-06-09 20:50:29 +0800

    bug fixes

--- authentication/authentication.py
+++ authentication/authentication.py
@@ -54,26 +54,37 @@ def get_user(user_id: str):
     return user
 
 @router.put("/update_user/{user_id}")
-def update_user(user_id: int, user: User):
+def update_user(user_id: str, user: User):
     conn = get_connection()
     cursor = conn.cursor()
 
     cursor.execute("""
-    UPDATE users WHERE user_id = %s""", user_id)
+    UPDATE users set username= %(username)s, 
+    password= %(password)s, 
+    email= %(email)s, 
+    first_name= %(first_name)s, 
+    last_name= %(last_name)s
+    WHERE user_id = %(id)s""",
+                   {'id': user_id,
+                    'first_name': user.first_name,
+                    'last_name': user.last_name,
+                    'email': user.email,
+                    'username': user.username,
+                    'password': user.password})
 
-    cursor.commit()
+    conn.commit()
     conn.close()
 
     return {f"message": f"{user.username} updated successfully"}
 
 @router.delete("/delete_uesr/{user_id}")
-def delete_user(user_id: int):
+def delete_user(user_id: str):
     conn = get_connection()
     cursor = conn.cursor()
 
     cursor.execute("""
-    DELETE FROM users WHERE user_id = %s""", user_id)
+    DELETE FROM users WHERE user_id = %(id)s""", {'id': user_id})
 
-    cursor.commit()
+    conn.commit()
     conn.close()
     return {"message": f"{user_id} deleted successfully"}
