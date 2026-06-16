bd1580e929800b50526e15234ed569a0eadb4468
Gyoza-Gyoza <calebwjle@gmail.com>
2026-06-17 00:58:43 +0800

    Update login.py

--- authentication/login.py
+++ authentication/login.py
@@ -22,21 +22,28 @@ def login(request: LoginRequest):
     conn = get_connection()
     cursor = conn.cursor()
 
-    print(request)
-    cursor.execute("""
-    SELECT password FROM users 
-    WHERE username = %s""",
-                        (request.username,))
-
-    storedPassword = cursor.fetchone()
-
     try:
-        passwordHasher.verify(storedPassword['password'], request.password)
+        cursor.execute("""
+            SELECT password FROM users 
+            WHERE username = %s""",
+                       (request.username,))
 
+        user = cursor.fetchone()
+        passwordHasher.verify(user['password'], request.password)
+
+    except TypeError as e:
+        print(type(e))
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Incorrect credentials")
     except VerifyMismatchError:
         raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,
                             detail = "Incorrect credentials")
     except Exception as e:
         print(type(e))
         print(e)
+
+    finally:
+        cursor.close()
+        conn.close()
+
     return {"message": "Login successful"}
