bf10c8569b0d5b353ef8f4de6cbdefbd1343b07f
Gyoza-Gyoza <calebwjle@gmail.com>
2026-07-07 00:11:36 +0800

    fixed? hopefully

--- data/database.py
+++ data/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 def get_connection():
     conn = psycopg.connect(
         dbname=os.getenv("DATABASE_NAME"),
-        user="postgres",
+        user=os.getenv("DATABASE_USER"),
         password=os.getenv("DATABASE_PASSWORD"),
         host=os.getenv("DATABASE_HOST"),
         port=os.getenv("DATABASE_PORT"),
