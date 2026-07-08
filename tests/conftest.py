ae701d93df3dceb746aef24c6465985009c8d969
Gyoza-Gyoza <calebwjle@gmail.com>
2026-07-08 18:27:22 +0800

    quick update

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,20 +8,23 @@ from fastapi.testclient import TestClient
 from main import app
 
 @pytest.fixture
-def test_receipt(test_item, test_user):
+def created_user(test_user):
     client = TestClient(app)
     response = client.post(
         "/users",
         json = test_user.model_dump(mode = "json"))
-    assert response.status_code == 201
-    user = response.json()
 
-    return Receipt(owner_id = user['user_id'],
+    yield response.json()
+
+    client.delete(f"/users/{response.json()['user_id']}")
+
+@pytest.fixture
+def test_receipt(test_item, created_user):
+    return Receipt(owner_id = created_user['user_id'],
                    title = "Unit Testing Test Receipt",
                    date = datetime.now(),
                    items = [test_item],
                    )
-# ensure delete endpoints delete all rows that depend on it 
 @pytest.fixture
 def test_item():
     return Item(title = "Unit Testing Test Item",
