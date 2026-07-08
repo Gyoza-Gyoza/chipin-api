d26205b570eca908f65724d72112c60ed1de7500
Gyoza-Gyoza <136580712+Gyoza-Gyoza@users.noreply.github.com>
2026-07-08 22:39:05 +0800

    wip

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,20 +3,9 @@ import pytest
 from datetime import datetime
 from authentication.users import User
 from data.items import Item
-from data.receipts import Receipt
+from data.receipts import Receipt, ReceiptData
 from fastapi.testclient import TestClient
 from main import app
-
-@pytest.fixture
-def created_user(test_user):
-    client = TestClient(app)
-    response = client.post(
-        "/users",
-        json = test_user.model_dump(mode = "json"))
-
-    yield response.json()
-
-    client.delete(f"/users/{response.json()['user_id']}")
 
 @pytest.fixture
 def test_receipt(test_item, created_user):
@@ -25,6 +14,29 @@ def test_receipt(test_item, created_user):
                    date = datetime.now(),
                    items = [test_item],
                    )
+@pytest.fixture
+def test_receipt_data(test_item, created_user, created_receipt, test_receipt):
+    total_cost = 0
+    for item in created_receipt.items:
+        total_cost += item.amount
+    return ReceiptData(receipt_id = created_receipt['receipt_id'],
+                       title = created_receipt['title'],
+                       date = created_receipt['date'],
+                       amount = created_receipt['amount'],
+                       items = created_receipt['items'])
+@pytest.fixture
+def created_receipt(test_receipt):
+    client = TestClient(app)
+    full_receipt = test_receipt.model_dump(mode = "json")
+    response = client.post(
+        "/receipts",
+        json = full_receipt)
+
+    merged = response.json() | full_receipt
+
+    yield merged
+
+    client.delete(f"/receipts/{merged['receipt_id']}")
 @pytest.fixture
 def test_item():
     return Item(title = "Unit Testing Test Item",
@@ -40,3 +52,16 @@ def test_user():
                 last_name = "unittesting",
                 phone_number = "91234567"
                 )
+@pytest.fixture
+def created_user(test_user):
+    client = TestClient(app)
+    full_user = test_user.model_dump(mode = "json")
+    response = client.post(
+        "/users",
+        json = full_user)
+
+    merged = response.json() | full_user
+
+    yield merged
+
+    client.delete(f"/users/{merged['user_id']}")
--- tests/test_receipts.py
+++ tests/test_receipts.py
@@ -8,8 +8,12 @@ def test_create_receipt(test_receipt):
         json = test_receipt.model_dump(mode = "json"))
     assert response.status_code == 201
 
-# def test_get_receipt(test_receipt):
-#     client = TestClient(app)
-#     response = client.get("/receipts/")
-#
-#     assert response.status_code == 200
+def test_get_receipt(created_receipt, test_receipt_data):
+    client = TestClient(app)
+    response = client.get(f"/receipts/{created_receipt['receipt_id']}")
+
+    assert response.status_code == 200
+    assert response.json() == test_receipt_data
+    # assert response.json()['receipt_id'] == created_receipt['receipt_id']
+    # assert response.json()['amount'] == created_receipt['amount']
+    # assert response.json()['items'] == created_receipt['items']
