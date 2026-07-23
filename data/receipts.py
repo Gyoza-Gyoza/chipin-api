c83ed6905c679317cb7488a30b88f990bed14adf
Gyoza-Gyoza <calebwjle@gmail.com>
2026-07-23 14:09:26 +0800

    testing broken code

--- data/receipts.py
+++ data/receipts.py
@@ -136,6 +136,7 @@ def get_all_receipts_by_id(user_id: int):
                 if sharer:
                     if sharer not in sharer_ids:
                         sharer_ids.append(sharer)
+            alskjdh
 
             result.append(ReceiptData(receipt_id=receipt['receipt_id'],
                                       owner_id=receipt['owner_id'],
